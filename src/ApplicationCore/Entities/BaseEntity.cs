b6a1e33034991b70332f3317d833e2fe4b4e1ee1
shengjie_yan <ysjshengjie@live.cn>
2018-08-18 18:41:28 +0800

    Add generic  BaseEntity

--- src/ApplicationCore/Entities/BaseEntity.cs
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -1,9 +1,9 @@
-﻿namespace Microsoft.eShopWeb.ApplicationCore.Entities
-{
+﻿namespace Microsoft.eShopWeb.ApplicationCore.Entities {
     // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
     // Using non-generic integer types for simplicity and to ease caching logic
-    public class BaseEntity
-    {
-        public int Id { get; set; }
+    public class BaseEntity : BaseEntity<int> { }
+
+    public class BaseEntity<T> {
+        public T Id { get; set; }
     }
 }
